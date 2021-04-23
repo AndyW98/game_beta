cdad9e8c05411e4324be9289a01af6fcc60305ce
Jerry Quijote <jquijot1@gmail.com>
2021-04-23 15:21:23 -0400

    Change bag to inventory, Add comments

--- Character/character.py
+++ Character/character.py
@@ -6,9 +6,10 @@ from Item.Weapon.weapon import Weapon
 
 class Character():
 
-    '''
-    A character in the game
-    '''
+    """A character in the game"""
+
+    stat_list = ['str', 'dex', 'con', 'int', 'wis', 'cha']
+
     def __init__(self, args):
         # __init__ is the function that is called when an
         # object is declared. Typically, you'll want to put
@@ -16,19 +17,19 @@ class Character():
         # parameters
         self.name = args['name']
         self.level = args['level']
-        self.health = args['health']
         self.base_stats = args['base_stats']
 
-        self.bag = Inventory()
+        self.inventory = Inventory()
         self.weapon = Weapon() # Held weapon
 
         # Derived Stats
         self.max_health = 2 * self.base_stats['con'] * self.level
         self.health = self.max_health
         self.exp = 200 * (self.level - 1) * self.level
-        self.carry_cap = 5 * args['base_stats']['str'] # Carrying Capacity
+        self.carry_cap = 5 * self.base_stats['str'] # Carrying Capacity
 
         self.damage = 2 * (self.base_stats['str'] - 10)
+        self.magic = 2 * (self.base_stats['int'] - 10)
         self.defense = round(0.5 * (self.base_stats['dex'] - 10))
 
     def __str__(self):
@@ -43,17 +44,17 @@ class Character():
                 " / " + str(self.max_health) + "\n" + \
                 "--------------------\n" + \
                 "Stats -\n" + \
-                "  Strength:     " + str(self.base_stats["str"]) + "\n" + \
-                "  Dexterity:    " + str(self.base_stats["dex"]) + "\n" + \
-                "  Constitution: " + str(self.base_stats["con"]) + "\n" + \
-                "  Intelligence: " + str(self.base_stats["int"]) + "\n" + \
-                "  Wisdom:       " + str(self.base_stats["wis"]) + "\n" + \
-                "  Charisma:     " + str(self.base_stats["cha"]) + "\n" + \
+                "  Strength:     " + str(self.base_stats['str']) + "\n" + \
+                "  Dexterity:    " + str(self.base_stats['dex']) + "\n" + \
+                "  Constitution: " + str(self.base_stats['con']) + "\n" + \
+                "  Intelligence: " + str(self.base_stats['int']) + "\n" + \
+                "  Wisdom:       " + str(self.base_stats['wis']) + "\n" + \
+                "  Charisma:     " + str(self.base_stats['cha']) + "\n" + \
                 str(self.weapon) + \
                 "--------------------\n" + \
-                "Inventory - [" + str(self.bag.get_weight()) + \
+                "Inventory - [" + str(self.inventory.get_weight()) + \
                     " / " + str(self.carry_cap) + "]\n" + \
-                str(self.bag)
+                str(self.inventory)
 
         return stats
 
@@ -65,11 +66,11 @@ class Character():
 
     # Printers
 
-    def print_bag(self):
+    def print_inventory(self):
         print("Inventory Weight: " + \
-              str(self.bag.get_weight()) + " / " + \
+              str(self.inventory.get_weight()) + " / " + \
               str(self.carry_cap))
-        print(self.bag)
+        print(self.inventory)
 
     # Getters
 
@@ -82,8 +83,8 @@ class Character():
     def get_weapon(self):
         return self.weapon
 
-    # Gets the stats of the caracter
     def get_stats(self):
+        """Returns the character's stats"""
         my_dict = {
             'name': self.name,
             'level': self.level,
@@ -91,9 +92,10 @@ class Character():
             'max_health': self.max_health,
             'base_stats': self.base_stats
         }
-        return json.dumps(my_dict, indent=2)
+        return my_dict
 
-    # Prints the character's stat prettily
+    # Printers
+
     def pretty_print_stats(self):
         stats = self.__str__()
         print(stats)
@@ -102,9 +104,11 @@ class Character():
     # Setters
 
     def set_name(self, name):
+        """Sets character's name"""
         self.name = name
 
     def set_weapon(self, weapon):
+        """Sets character's equipped weapon"""
         self.weapon = weapon
 
     # Functions
@@ -116,6 +120,13 @@ class Character():
     # the corresponding base stat
 
     def level_up(self, stat1, stat2, level_add=1):
+        """Levels up character
+
+        Keyword arguments:
+        stat1     -- 1st stat to increase
+        stat2     -- 2nd stat to increase
+        level_add -- number of levels to add
+        """
         self.level += level_add
         
         self.max_health += int(0.5 * self.base_stats['con'])
@@ -126,27 +137,50 @@ class Character():
         self.base_stats[stat1] += level_add
         self.base_stats[stat2] += level_add
 
-    # Adds item to the character's inventory
-    # Expects an Item object
     def add_item(self, item):
-        self.bag.add_item(item)
+        """Adds item to the character's inventory
 
-    # Expects that weapon is a Weapon
+        Keyword arguments:
+        item -- an Item object
+        """
+        self.inventory.add_item(item)
+
     def equip_weapon(self, weapon):
-        if (weapon in self.bag.get_bag()):
+        """Equips weapon to character
+
+        Keyword arguments:
+        weapon -- a Weapon object
+        """
+        if (weapon in self.inventory.get_bag()):
             self.set_weapon(weapon)
 
+    def attack(self, character):
+        """Attacks another character
 
-    # ------------------------------------------------
-    # Deal Damage
-    # ------------------------------------------------
-    # Expects
+        Keyword arguments:
+        character -- another Character
+        """
+        if not isinstance(character, Character):
+            # Character's cannot attack non-characters
+            print("You can't attack that!")
+        else:    
+            damage = self.deal_damage()
+            character.take_damage(damage)
+
     def deal_damage(self, modifiers=1):
-        
-        damage = modifiers * (self.damage + self.weapon.get_damage())
+        """Deals damage to something
+
+        Keyword arguments:
+        modifiers -- damage modifiers (default 1)
+        """    
+        damage = modifiers * (self.damage + self.weapon.get_power())
 
         return damage
 
-    # Expects incoming unmodified damage
     def take_damage(self, incoming_damage):
+        """Decreases Character's health
+        
+        Keyword arguments:
+        incoming_damage -- damage to be dealt to character
+        """
         self.health -= max(0, (incoming_damage - self.get_defense()))
