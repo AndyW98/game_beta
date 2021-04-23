2952b296d6f18cfb20625b5929d3d07d291c94e3
Jerry Quijote <jquijot1@gmail.com>
2021-04-23 19:00:01 -0400

    Add comments, change constructor

--- Character/Inventory/inventory.py
+++ Character/Inventory/inventory.py
@@ -6,8 +6,8 @@ class Inventory():
     This is a Character's inventory
     '''
     # Constructor
-    def __init__(self):
-        self.bag = []
+    def __init__(self, items=[]):
+        self.bag = items
         self.current_weight = 0
 
     # String representation
@@ -17,21 +17,35 @@ class Inventory():
     # Getters
 
     def get_weight(self):
+        """Returns the weight of the inventory"""
         return self.current_weight
 
     def get_bag(self):
+        """Returns the bag of the inventory"""
         return self.bag
 
-    # Add item
-    # Expects an Item object
     def add_item(self, item):
-        self.bag.append(item)
-        self.current_weight += item.get_weight()
+        """Adds item to the inventory
 
-    # Remove item
-    # Expects an Item object
+        Keyword arguments:
+        item -- an Item object
+        """
+        if isinstance(item, Item):
+            self.bag.append(item)
+            self.current_weight += item.get_weight()
+        else:
+            print(str(item) + "is not an item!")
+
     def remove_item(self, item):
-        self.bag.remove(item)
-        self.current_weight -= item.get_weight()
+        """Removes item from the inventory
+
+        Keyword arguments:
+        item -- an Item object
+        """
+        if (isinstance(item, Item) and item in self.bag):
+            self.bag.remove(item)
+            self.current_weight -= item.get_weight()
+        else:
+            print(str(item) + " is not in the bag!")
     
     
